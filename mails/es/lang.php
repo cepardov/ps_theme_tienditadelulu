ef4c51b547f9c9160192e5b8b5753c3f5d1a9479
Cristian Pardo <cristian.pardo@externos.bci.cl>
2021-11-13 14:02:06 -0300

    init commit

---
+++ mails/es/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+global $_LANGMAIL;
+$_LANGMAIL = array();
+$_LANGMAIL['Product out of stock'] = '';
+$_LANGMAIL['Your order has been changed'] = '';
+$_LANGMAIL['New order : #%d - %s'] = '';
+$_LANGMAIL['New return from order #%d - %s'] = '';
+$_LANGMAIL['Product available'] = '';
+$_LANGMAIL['Stock coverage'] = '';
+
+?>
